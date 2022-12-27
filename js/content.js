ca1ff38827b808ead46b852991b2e52ec89db9d1
muoio <tuana2tqt@gmail.com>
2022-12-28 06:12:42 +0700

    Add files via upload

--- js/content.js
+++ js/content.js
@@ -1,6 +1,8 @@
 let next_page_btn;
+let prev_page_btn;
 let start_btn;
-
+let actions;
+let panel;
 
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -13,10 +15,8 @@ async function wait_for_element(selector){
     if (max_wait<=0) return false;
     else return true;
 }
-function delete_all_btn(){
-    let start_btn = document.createElement('button');
-    start_btn.textContent = 'Xóa tất cả';
-    start_btn.onclick = function(){
+async function click_to_end(){
+    new Promise(resolve=>{
         let click_next = setInterval(function(){
             next_page_btn.click();
             if (next_page_btn.parentElement.disabled) chain++;
@@ -24,17 +24,62 @@ function delete_all_btn(){
             if(chain > 100) {
                 clearInterval(click_next);
                 alert("end");
+                resolve(true);
+                return true;
             }
         },100);
-    }
+    });
+}
+
+async function delete_post(){
+    //return new Promise(async resolve=>{
+        prev_page_btn = document.querySelectorAll("[type='submit'][value='1'] i")[2];
+        next_page_btn  = document.querySelectorAll("[type='submit'][value='1'] i")[3];
+
+        prev_page_btn.click();
+        await sleep(500);
+        let checkbox = document.querySelector('[role="checkbox"]');
+        checkbox.click();
+        await sleep(500);
+
+        panel = next_page_btn.parentElement.parentElement.parentElement;
+        actions = panel.parentElement.parentElement.firstChild.querySelector('i');
+        
+        actions.click();
+        await sleep(500);
+        let item1 = document.querySelector('.__MenuItem');
+        item1.click();
+        await sleep(500);
+        let confirms = document.querySelectorAll('a.layerCancel.selected');
+        let confirm = confirms[confirms.length-1];
+        confirm.click();
+    //     resolve(true);
+    //     return true;
+    // });
+}
+
+async function process(){
+    //await click_to_end();
+    //while(!prev_page_btn.disabled)
+    setInterval(delete_post,10000);
+        //if (prev_page_btn.disabled) clearInterval(delete_all);
+    //},5000);
+}
+
+function delete_all_btn(){
+    let start_btn = document.createElement('button');
+    start_btn.textContent = 'Xóa tất cả';
+    start_btn.onclick = process;
 
     return start_btn;
 }
 window.onload = function(){
+    prev_page_btn = document.querySelectorAll("[type='submit'][value='1'] i")[2];
     next_page_btn  = document.querySelectorAll("[type='submit'][value='1'] i")[3];
-    sleep(5000);
     next_page_btn.click();
-    let panel = next_page_btn.parentElement.parentElement.parentElement
-    console.log(panel);
+    panel = next_page_btn.parentElement.parentElement.parentElement;
+    actions = panel.parentElement.parentElement.firstChild.querySelector('i');
+    //console.log(panel);
+    console.log(actions);
     panel.appendChild(delete_all_btn());
 }
